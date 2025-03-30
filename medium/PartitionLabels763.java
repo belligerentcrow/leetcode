d749922eb0cea550be2f3e2c9186dc6c343761b0
belligerentcrow <74275837+belligerentcrow@users.noreply.github.com>
2025-03-30 16:01:41 +0200

    Fix solution

--- medium/PartitionLabels763.java
+++ medium/PartitionLabels763.java
@@ -3,23 +3,22 @@ import java.util.List;
 import java.util.ArrayList;
 
 class PartitionLabels763{
-    HashMap<Integer, Character> charToLastPos = new HashMap<Integer,Character>();
+    HashMap<Character,Integer> charToLastPos = new HashMap<Character,Integer>();
 
     public List<Integer> solution(String s){
         List<Integer> sol = new ArrayList<>();
 
-        for(int i = 0; i < s.length(); i++){
-            charToLastPos.put(i,s.charAt(i));
-            System.out.println("i: "+i+", charAtI: "+s.charAt(i));
-            System.out.println("CharToLastPos.get(i) = "+charToLastPos.get(i));
+        for(int i =0; i <s.length(); i++){
+            charToLastPos.put(s.charAt(i), i);
         }
-        
+
+        System.out.println(charToLastPos);
         int size=0;
         int end =0;
-        for(int i=0; i < charToLastPos.size(); i++){
+        for(int i=0; i < s.length(); i++){
             size+=1;
-            if(charToLastPos.get(i)>end){
-                end = charToLastPos.get(i);
+            if(charToLastPos.get(s.charAt(i))>end){
+                end = charToLastPos.get(s.charAt(i));
             }   
             if(i==end){
                 sol.add(size);
